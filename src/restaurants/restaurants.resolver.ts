7c9104983ab8de6b800a60fdcf43c78dc951e5ea
sik <jsjsjskjs@gmail.com>
2022-03-07 16:54:25 +0900

    grapql arguments

--- src/restaurants/restaurants.resolver.ts
+++ src/restaurants/restaurants.resolver.ts
@@ -1,12 +1,13 @@
-import { Query } from '@nestjs/graphql'
+import { Args, Query } from '@nestjs/graphql'
 import { Resolver } from '@nestjs/graphql'
 import { Restuarnt } from './entities/restaurant.entity'
 
 @Resolver(() => Restuarnt)
 export class RestaurantResolver {
-  @Query(() => Restuarnt)
-  myRestaurant() {
-    return true
+  @Query(() => [Restuarnt]) //배열을 리턴한다는 뜻 => graphql에선 밖에 씌워준다
+  restaurant(@Args('veganOnly') veganOnly: boolean): Restuarnt[] { //배열을 리턴한다는 뜻 => typescript에선 entity바로 뒤에 []를 넣어준다
+    console.log(veganOnly)
+    return []
   }
   
 }
