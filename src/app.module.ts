28b7dc11bdee4d9b575d769c3a4a5271013e85f7
sik <jsjsjskjs@gmail.com>
2022-03-07 13:47:53 +0900

    apollo server set & make first resolver

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo'
+import { Module } from '@nestjs/common'
+import { GraphQLModule } from '@nestjs/graphql'
+import { join } from 'path'
+import { RestaurantsModule } from './restaurants/restaurants.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [GraphQLModule.forRoot<ApolloDriverConfig>({
+    driver: ApolloDriver,
+    //autoSchemaFile: join(process.cwd(), 'src/schema.gql')로 지정하면 schema.gql 파일이 자동생성 되지만
+    //nestjs는 resoler.ts 파일에서 관리가 가능하기 때문에 true로 변경해주면 파일을 직접 갖고 있지 않을 수 있도록 할 수 있다.
+    autoSchemaFile: true
+  }), RestaurantsModule],
+  controllers: [],
+  providers: []
 })
 export class AppModule {}
